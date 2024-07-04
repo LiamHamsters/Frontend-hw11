e635bfe1780a29830bbdacdf320e04efe9b1fe70
LIAM <bogdangallka@gmail.com>
2024-07-04 16:33:38 +0300

    test2

---
+++ frontend/src/components/main/Index.vue
@@ -0,0 +1,21 @@
+<!--WORKING-->
+<template>
+  <div>
+    iii
+    <Post v-for="post in posts" :post="post" :key="post.id" />
+  </div>
+</template>
+
+<script>
+import Post from "@/components/page/Post.vue";
+
+export default {
+  name: "Index",
+  components: {Post},
+  props: ["posts"]
+}
+</script>
+
+<style scoped>
+
+</style>
